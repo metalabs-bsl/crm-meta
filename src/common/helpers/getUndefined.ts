8dd6e4ef211a474804c7c468388f146f9ece70bf
Mikhaylov-M <mikhaylov.m@outlook.com>
2024-06-27 01:29:54 +0600

    some fix

--- src/common/helpers/getUndefined.ts
+++ src/common/helpers/getUndefined.ts
@@ -1,4 +1,4 @@
 // Эта функция явно возвращает `undefined`. Полезно для ясности в коде, целей тестирования или когда требуется значение undefined.
-export const getUndefined = () => {
+export const getUndefined = (): undefined => {
   return;
 };
