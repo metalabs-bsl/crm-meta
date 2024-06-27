16e03df87312131e28db5e3e954e4aece4b38a10
IsaRakhatbekov <97755203+IsaRakhatbekov@users.noreply.github.com>
2024-06-27 13:25:32 +0600

    New document page (#20)
    * commiting to pull dev
    
    * chenged document page
    
    * fix green upload btn, added modal for delete and fix scroll-y of table
    
    * fix download in modal, and need to fix checkbox in document-table and in original-table
    
    * fixed the variables

--- src/pages/Document/DocumentTable/DocumentTable.tsx
+++ src/pages/Document/DocumentTable/DocumentTable.tsx
@@ -1,8 +1,13 @@
-import { FC } from 'react';
+import { FC, useCallback, useState } from 'react';
+import { Checkbox } from 'common/ui';
+import { DeleteModal } from 'common/components';
+import { DownloadDelete } from '../DownloadDelete';
 import { DocumentTableRow } from './DocumentTableRow';
 import styles from './Document.module.scss';
 
 interface DocumentData {
+  id: string;
+  name: string;
   title: string;
   file: string;
 }
@@ -12,18 +17,52 @@ interface IProps {
 }
 
 export const DocumentTable: FC<IProps> = ({ data }) => {
+  const [selectedRows, setSelectedRows] = useState<number[]>([]);
+  const [selectAll, setSelectAll] = useState(false);
+  const [openDeleteModal, setOpenDeleteModal] = useState<boolean>(false);
+
+  const handleSelectAll = useCallback(() => {
+    setSelectAll((prev) => !prev);
+    setSelectedRows(() => (!selectAll ? data.map((_, index) => index) : []));
+  }, [selectAll, data]);
+
+  const handleSelectRow = useCallback((index: number) => {
+    setSelectedRows((prev) => (prev.includes(index) ? prev.filter((i) => i !== index) : [...prev, index]));
+  }, []);
+
+  const handleDownload = () => {
+    console.log('Скачать', selectedRows);
+  };
+
+  const handleDelete = useCallback(() => {
+    setOpenDeleteModal(true);
+  }, []);
+
   return (
-    <div className={styles.table}>
-      <div className={styles.thead}>
-        <div className={`${styles.headTd} ${styles.naming}`}>Название</div>
-        <div className={`${styles.headTd} ${styles.format}`}>Формат</div>
-        <div className={`${styles.headTd} ${styles.action}`}>действие</div>
+    <div className={styles.tableContainer}>
+      <div className={styles.table}>
+        <div className={styles.thead}>
+          <div className={styles.theadCheckbox}>
+            <Checkbox checked={selectAll} onChange={handleSelectAll} />
+          </div>
+          <div className={`${styles.headTd} ${styles.id}`}>номер договора</div>
+          <div className={`${styles.headTd} ${styles.name}`}>ФИО</div>
+          <div className={`${styles.headTd} ${styles.naming}`}>название договора</div>
+          <div className={`${styles.headTd} ${styles.format}`}>формат</div>
+        </div>
+        <div className={styles.tbody}>
+          {data.map((el, index) => (
+            <DocumentTableRow key={index} index={index} data={el} isSelected={selectedRows.includes(index)} onSelectRow={handleSelectRow} />
+          ))}
+        </div>
       </div>
-      <div className={styles.tbody}>
-        {data.map((el, index) => (
-          <DocumentTableRow key={index} data={el} />
-        ))}
-      </div>
+      {selectedRows.length > 0 && <DownloadDelete onDelete={handleDelete} onDownload={handleDownload} />}
+      <DeleteModal
+        isOpen={openDeleteModal}
+        onCancel={() => setOpenDeleteModal(false)}
+        text={`Вы уверены, что хотите удалить счёт "${selectedRows}"?`}
+        onDelete={handleDelete}
+      />
     </div>
   );
 };
