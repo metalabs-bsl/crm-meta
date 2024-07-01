c329b7f24d23d62f5b6b6ff70e39cb938beb0bbf
Tariel <tarieltairov1@gmail.com>
2024-07-01 17:18:14 +0300

    fix: notification layout

--- src/common/components/BreakModal/BreakModal.tsx
+++ src/common/components/BreakModal/BreakModal.tsx
@@ -4,19 +4,18 @@ import { Modal } from '../Modal';
 import styles from './styles.module.scss';
 
 interface IProps {
-  data?: string;
   isOpen?: boolean;
   onCancel?: () => void;
 }
 
-export const BreakModal: FC<IProps> = ({ isOpen = false, onCancel, data }) => {
+export const BreakModal: FC<IProps> = ({ isOpen = false, onCancel }) => {
   return (
     <Modal isOpen={isOpen} onClose={onCancel}>
       <div className={styles.modalContent}>
         <Icon type='break' className={styles.icon} />
         <div className={styles.data}>
           <p className={styles.name_wrapper}>
-            До окончания перерыва <br /> осталось <span className={styles.name}>{data}</span>
+            До окончания перерыва <br /> осталось 15 минут
           </p>
         </div>
       </div>
--- src/common/containers/NotificationLayout/NotificationLayout.tsx
+++ src/common/containers/NotificationLayout/NotificationLayout.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode, useState } from 'react';
-import { BirthDayModal, BreakModal } from 'common/components';
+import { BirthDayModal, BreakModal, GreetingsModal } from 'common/components';
 import { NoteModal } from 'common/components/NoteModal';
-import { birthdayData, breakData, noteData } from './NotificationLayout.helper';
+import { birthdayData, noteData } from './NotificationLayout.helper';
 
 import { NOTIFICATION_COMPONENTS } from 'types/enums';
 
@@ -10,18 +10,48 @@ interface IProps {
 }
 
 export const NotificationLayout: FC<IProps> = ({ children }) => {
-  const notificationType = NOTIFICATION_COMPONENTS.NOTE;
+  const notificationType = NOTIFICATION_COMPONENTS.BREAK;
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [prevModal, setPrevModal] = useState<boolean>(false);
+  // const [isPreved, setIsPreved] = useState<boolean>(false);
 
-  const handleCancel = () => {
+  // const { userInfo } = useAppSelector(employeesSelectors.employees);
+
+  const closeNotificationModal = () => {
     setIsModalOpen(false);
+    // setIsPreved(false);
+  };
+
+  // const openNotificationModal = () => {
+  //   setIsModalOpen(true);
+  //   const audio = new Audio('/notification.mp3');
+  //   audio.play().catch((error) => {
+  //     console.error('Audio playback failed:', error);
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   if (userInfo) {
+  //     setPrevModal(true);
+  //   }
+  // }, [userInfo]);
+
+  // useEffect(() => {
+  //   if (userInfo && isPreved) {
+  //     openNotificationModal();
+  //   }
+  // }, [isPreved, userInfo]);
+
+  const closePrevModal = () => {
+    setPrevModal(false);
+    // setIsPreved(true);
   };
 
   const getNotificationComponents = (type: NOTIFICATION_COMPONENTS) => {
     const modals: Record<NOTIFICATION_COMPONENTS, ReactNode> = {
-      [NOTIFICATION_COMPONENTS.BIRTHDAY]: <BirthDayModal isOpen={isModalOpen} onCancel={handleCancel} data={birthdayData} />,
-      [NOTIFICATION_COMPONENTS.NOTE]: <NoteModal isOpen={isModalOpen} onCancel={handleCancel} data={noteData} />,
-      [NOTIFICATION_COMPONENTS.BREAK]: <BreakModal isOpen={isModalOpen} onCancel={handleCancel} data={breakData} />
+      [NOTIFICATION_COMPONENTS.BIRTHDAY]: <BirthDayModal isOpen={isModalOpen} onCancel={closeNotificationModal} data={birthdayData} />,
+      [NOTIFICATION_COMPONENTS.NOTE]: <NoteModal isOpen={isModalOpen} onCancel={closeNotificationModal} data={noteData} />,
+      [NOTIFICATION_COMPONENTS.BREAK]: <BreakModal isOpen={isModalOpen} onCancel={closeNotificationModal} />
     };
     return modals[type];
   };
@@ -30,6 +60,13 @@ export const NotificationLayout: FC<IProps> = ({ children }) => {
     <div>
       {children}
       {getNotificationComponents(notificationType)}
+      {/* <Modal isOpen={prevModal} onClose={closePrevModal}>
+        <p style={{ textAlign: 'center', fontSize: '25px' }}>
+          С возвращением <br />
+          {userInfo?.first_name} {userInfo?.second_name}
+        </p>
+      </Modal> */}
+      {prevModal && <GreetingsModal isOpen={prevModal} onCancel={closePrevModal} />}
     </div>
   );
 };
