86cc761b05769d64f2e86b4fa62532a19018f6cc
Tariel <tarieltairov1@gmail.com>
2024-07-02 19:25:39 +0300

    fix: card todo create form

--- src/pages/CRM/Deals/Kanban/Card/TodoCreateForm/TodoCreateForm.tsx
+++ src/pages/CRM/Deals/Kanban/Card/TodoCreateForm/TodoCreateForm.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { Button, DatePicker } from 'common/ui';
+import { Button, DatePicker, Loading } from 'common/ui';
 import { useNotify } from 'common/hooks';
 import { MESSAGE } from 'common/constants';
 import { useCreateReminderMutation } from 'api/admin/leads/leads.api';
@@ -21,7 +21,7 @@ export const TodoCreateForm: FC<IProps> = ({ lead_id, onCancel }) => {
     formState: { errors }
   } = useForm<ICreateReminderParams>();
   const notify = useNotify();
-  const [cretateReminder] = useCreateReminderMutation();
+  const [cretateReminder, { isLoading }] = useCreateReminderMutation();
 
   const onSubmit = (data: ICreateReminderParams) => {
     const updatedData = { ...data, lead_id, status: 1 };
@@ -30,24 +30,29 @@ export const TodoCreateForm: FC<IProps> = ({ lead_id, onCancel }) => {
       .then(() => {
         notify(MESSAGE.CREATED, 'success');
         onCancel();
+      })
+      .catch(() => {
+        notify(MESSAGE.ERROR, 'error');
       });
   };
 
   return (
-    <form className={styles.textareaBlock} onSubmit={handleSubmit(onSubmit)}>
-      <div className={styles.wrapper}>
-        <textarea
-          {...register('reminder_text', { required: 'Название доски обязательно' })}
-          placeholder='Напишите что нужно сделать'
-        ></textarea>
-        {errors.reminder_text && <span className={styles.errorMessage}>{errors.reminder_text.message}</span>}
-        <DatePicker {...register('date_to_finish', { required: 'Название доски обязательно' })} className={styles.datePicker} />
-        {errors.date_to_finish && <span className={styles.errorMessage}>{errors.date_to_finish.message}</span>}
-      </div>
-      <div className={styles.modalBtnWrapper}>
-        <Button text='сохранить' styleType={BUTTON_TYPES.YELLOW} type='submit' />
-        <Button text='отменить' styleType={BUTTON_TYPES.Link_BLACK} type='button' onClick={onCancel} />
-      </div>
-    </form>
+    <Loading isSpin={isLoading}>
+      <form className={styles.textareaBlock} onSubmit={handleSubmit(onSubmit)}>
+        <div className={styles.wrapper}>
+          <textarea
+            {...register('reminder_text', { required: 'Название доски обязательно' })}
+            placeholder='Напишите что нужно сделать'
+          ></textarea>
+          {errors.reminder_text && <span className={styles.errorMessage}>{errors.reminder_text.message}</span>}
+          <DatePicker {...register('date_to_finish', { required: 'Название доски обязательно' })} className={styles.datePicker} />
+          {errors.date_to_finish && <span className={styles.errorMessage}>{errors.date_to_finish.message}</span>}
+        </div>
+        <div className={styles.modalBtnWrapper}>
+          <Button text='сохранить' styleType={BUTTON_TYPES.YELLOW} type='submit' />
+          <Button text='отменить' styleType={BUTTON_TYPES.Link_BLACK} type='button' onClick={onCancel} />
+        </div>
+      </form>
+    </Loading>
   );
 };
