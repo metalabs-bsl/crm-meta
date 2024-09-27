9b41750d77f7a1f9994497e8239b6031f241dda1
AzatBaisynov <59169920+AzatBaisynov@users.noreply.github.com>
2024-09-27 13:59:52 +0600

    added new rows into start (#61)
    Co-authored-by: IsaRakhatbekov <isarahatbekov250@gmail.com>

---
+++ src/common/components/SettingsData/SettingsData.tsx
@@ -0,0 +1,97 @@
+import { useEffect, useRef, useState } from 'react';
+import { Icon } from 'common/ui';
+import styles from './Settings.module.scss';
+
+const initialValues = {
+  Conversion: 100,
+  Bonuses: 100,
+  Profit: 100,
+  PAX: 100,
+  AdditionalBonuses: 40,
+  CrmManagement: 20
+};
+
+// Определяем тип ключей объекта initialValues
+type SettingsType = keyof typeof initialValues;
+
+interface SettingsDataProps {
+  type: SettingsType; // указываем тип параметра type
+}
+
+export const SettingsData = ({ type }: SettingsDataProps) => {
+  const [value, setValue] = useState(initialValues[type]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [switchState, setSwitchState] = useState(false);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [originalValue, setOriginalValue] = useState(value);
+
+  useEffect(() => {
+    // Выводим в консоль текущее значение и состояние переключателя для соответствующего направления
+    console.log(`${type}: ${value}%, Переключатель: ${switchState ? 'Включен' : 'Выключен'}`);
+  }, [value, switchState, type]); // Каждый раз, когда значения меняются, срабатывает этот эффект
+
+  const handleBlur = () => {
+    if (isEditing) {
+      saveChanges();
+    }
+  };
+
+  const handleClickSwitchBtn = () => {
+    setSwitchState((prev) => !prev);
+  };
+
+  const saveChanges = () => {
+    setIsEditing(false);
+  };
+
+  const cancelChanges = () => {
+    setValue(originalValue);
+    setIsEditing(false);
+  };
+
+  const startEdit = () => {
+    setOriginalValue(value);
+    setIsEditing(true);
+    inputRef.current?.focus(); // Устанавливаем фокус на поле ввода
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = Number(e.target.value);
+    if (!isNaN(newValue)) {
+      setValue(newValue);
+    }
+  };
+
+  return (
+    <div className={styles.bonusesInner}>
+      <div className={styles.bonusesPercent}>
+        <input
+          ref={inputRef}
+          className={styles.bonusesInput}
+          type='text'
+          value={value}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          readOnly={!isEditing}
+        />
+        <span>%</span>
+      </div>
+      {isEditing ? (
+        <div className={styles.bonusesEditWrapper}>
+          <button className={`${styles.btn} ${styles.saveBtn}`} onClick={saveChanges}>
+            <Icon className={styles.saveSvg} type='done' />
+          </button>
+          <button className={`${styles.btn} ${styles.cancelBtn}`} onClick={cancelChanges}>
+            <Icon className={styles.cancelSvg} type='burger-close' />
+          </button>
+        </div>
+      ) : (
+        <Icon className={styles.bonusesEdit} type='edit' onClick={startEdit} />
+      )}
+      <button className={`${styles.switchWrapper} ${switchState ? styles.switchTrue : styles.switchFalse}`} onClick={handleClickSwitchBtn}>
+        <span className={`${styles.switch} ${switchState ? styles.switchTrue : styles.switchFalse}`}></span>
+      </button>
+    </div>
+  );
+};
--- src/pages/Settings/Settings.tsx
+++ src/pages/Settings/Settings.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { Icon, Loading } from 'common/ui';
+import { SettingsData } from 'common/components/SettingsData/SettingsData';
 import styles from './styles.module.scss';
 
 type MockDataType = {
@@ -8,23 +9,11 @@ type MockDataType = {
   isEditing: boolean;
 };
 
-const mockData = {
-  conversionPercent: 100 // например, 25%
-};
+// const mockData = {
+//   conversionPercent: 100 // например, 25%
+// };
 
 export const Settings = () => {
-  const [conversionSwitchBtn, setConversionSwitchBtn] = useState(false);
-  const [profitSwitchBtn, setProfitSwitchBtn] = useState(false);
-  const [paxSwitchBtn, setPaxSwitchBtn] = useState(false);
-  const [conversionPercent, setConversionPercent] = useState(mockData.conversionPercent);
-  const [conversionIsEditing, setConversionIsEditing] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [originalConversionPercent, setOriginalConversionPercent] = useState(mockData.conversionPercent);
-
-  const [bonusesPercent, setBonusesPercent] = useState(mockData.conversionPercent);
-  const [bonusesIsEditing, setBonusesIsEditing] = useState(false);
-  const bonusesInputRef = useRef<HTMLInputElement>(null);
-  const [originalBonusesPercent, setOriginalBonusesPercent] = useState(mockData.conversionPercent);
   // Состояние для блока "Бренды"
   const [brandData, setBrandData] = useState<MockDataType[]>([
     { id: 1, value: 'Бренд 1', isEditing: false },
@@ -119,61 +108,6 @@ export const Settings = () => {
       }
     });
   }, [brandData, serviceData]);
-
-  const handleClickConversionSwitchBtn = () => {
-    setConversionSwitchBtn((prev) => !prev);
-    console.log(conversionSwitchBtn);
-  };
-  const handleClickProfitSwitchBtn = () => {
-    setProfitSwitchBtn((prev) => !prev);
-    console.log(profitSwitchBtn);
-  };
-  const handleClickPaxSwitchBtn = () => {
-    setPaxSwitchBtn((prev) => !prev);
-    console.log(paxSwitchBtn);
-  };
-  const startConversionEdit = () => {
-    setOriginalConversionPercent(conversionPercent);
-    setConversionIsEditing(true);
-    inputRef.current?.focus(); // Устанавливаем фокус на поле ввода
-  };
-
-  const saveConversionChanges = () => {
-    // Логика для сохранения изменений
-    setConversionIsEditing(false);
-  };
-  const cancelConversionChanges = () => {
-    setConversionPercent(originalConversionPercent);
-    setConversionIsEditing(false);
-  };
-
-  const handleConversionBlur = () => {
-    if (conversionIsEditing) {
-      saveConversionChanges();
-    }
-  };
-
-  const startBonusesEdit = () => {
-    setOriginalBonusesPercent(bonusesPercent);
-    setBonusesIsEditing(true);
-    bonusesInputRef.current?.focus(); // Устанавливаем фокус на поле ввода
-  };
-
-  const saveBonusesChanges = () => {
-    // Логика для сохранения изменений
-    setBonusesIsEditing(false);
-  };
-
-  const cancelBonusesChanges = () => {
-    setBonusesPercent(originalBonusesPercent);
-    setBonusesIsEditing(false);
-  };
-
-  const handleBonusesBlur = () => {
-    if (bonusesIsEditing) {
-      saveBonusesChanges();
-    }
-  };
 
   return (
     <Loading>
@@ -274,6 +208,7 @@ export const Settings = () => {
               </div>
             </div>
           </div>
+
           <div className={styles.startWrapper}>
             <h2 className={styles.blocksTitle}>Старт</h2>
             <ul className={styles.startInner}>
@@ -282,38 +217,7 @@ export const Settings = () => {
                   <h4 className={`${styles.startInnerTitle} ${styles.conversionTitle}`}>Конверсия</h4>
                   <p className={`${styles.startInnerText} ${styles.conversionText}`}>Вы можете менять процент в конверсии KPI</p>
                 </div>
-                <div className={styles.conversionInner}>
-                  <div className={styles.conversionPercent}>
-                    <input
-                      ref={inputRef}
-                      className={styles.conversionInput}
-                      type='text'
-                      value={conversionPercent}
-                      onChange={(e) => setConversionPercent(Number(e.target.value))}
-                      onBlur={handleConversionBlur}
-                      readOnly={!conversionIsEditing}
-                    />
-                    <span>%</span>
-                  </div>
-                  {conversionIsEditing ? (
-                    <div className={styles.conversionEditWrapper}>
-                      <button className={`${styles.btn} ${styles.saveBtn}`} onClick={saveConversionChanges}>
-                        <Icon className={styles.saveSvg} type='done' />
-                      </button>
-                      <button className={`${styles.btn} ${styles.cancelBtn}`} onClick={cancelConversionChanges}>
-                        <Icon className={styles.cancelSvg} type='burger-close' />
-                      </button>
-                    </div>
-                  ) : (
-                    <Icon className={styles.conversionEdit} type='edit' onClick={startConversionEdit} />
-                  )}
-                  <button
-                    className={`${styles.switchWrapper} ${conversionSwitchBtn ? styles.switchTrue : styles.switchFalse}`}
-                    onClick={handleClickConversionSwitchBtn}
-                  >
-                    <span className={`${styles.switch} ${conversionSwitchBtn ? styles.switchTrue : styles.switchFalse}`}></span>
-                  </button>
-                </div>
+                <SettingsData type='Conversion' />
               </li>
 
               <li className={`${styles.startBlock} ${styles.bonuses}`}>
@@ -321,32 +225,7 @@ export const Settings = () => {
                   <h4 className={`${styles.startInnerTitle} ${styles.bonusesTitle}`}>Бонусы</h4>
                   <p className={`${styles.startInnerText} ${styles.bonusesText}`}>Вы можете менять проценты бонусов сотрудника</p>
                 </div>
-                <div className={styles.bonusesInner}>
-                  <div className={styles.bonusesPercent}>
-                    <input
-                      ref={bonusesInputRef}
-                      className={styles.bonusesInput}
-                      type='text'
-                      value={bonusesPercent}
-                      onChange={(e) => setBonusesPercent(Number(e.target.value))}
-                      onBlur={handleBonusesBlur}
-                      readOnly={!bonusesIsEditing}
-                    />
-                    <span>%</span>
-                  </div>
-                  {bonusesIsEditing ? (
-                    <div className={styles.bonusesEditWrapper}>
-                      <button className={`${styles.btn} ${styles.saveBtn}`} onClick={saveBonusesChanges}>
-                        <Icon className={styles.saveSvg} type='done' />
-                      </button>
-                      <button className={`${styles.btn} ${styles.cancelBtn}`} onClick={cancelBonusesChanges}>
-                        <Icon className={styles.cancelSvg} type='burger-close' />
-                      </button>
-                    </div>
-                  ) : (
-                    <Icon className={styles.bonusesEdit} type='edit' onClick={startBonusesEdit} />
-                  )}
-                </div>
+                <SettingsData type='Bonuses' />
               </li>
 
               <li className={`${styles.startBlock} ${styles.profit}`}>
@@ -354,12 +233,7 @@ export const Settings = () => {
                   <h4 className={`${styles.startInnerTitle} ${styles.profitTitle}`}>Прибыль</h4>
                   <p className={`${styles.startInnerText} ${styles.profitText}`}>Вы можете выключить - включить Прибыль, чтобы что-то...</p>
                 </div>
-                <button
-                  className={`${styles.switchWrapper} ${profitSwitchBtn ? styles.switchTrue : styles.switchFalse}`}
-                  onClick={handleClickProfitSwitchBtn}
-                >
-                  <span className={`${styles.switch} ${profitSwitchBtn ? styles.switchTrue : styles.switchFalse}`}></span>
-                </button>
+                <SettingsData type='Profit' />
               </li>
 
               <li className={`${styles.startBlock} ${styles.pax}`}>
@@ -367,12 +241,23 @@ export const Settings = () => {
                   <h4 className={`${styles.startInnerTitle} ${styles.paxTitle}`}>PAX</h4>
                   <p className={`${styles.startInnerText} ${styles.paxText}`}>Вы можете выключить - включить PAX, чтобы что-то...</p>
                 </div>
-                <button
-                  className={`${styles.switchWrapper} ${paxSwitchBtn ? styles.switchTrue : styles.switchFalse}`}
-                  onClick={handleClickPaxSwitchBtn}
-                >
-                  <span className={`${styles.switch} ${paxSwitchBtn ? styles.switchTrue : styles.switchFalse}`}></span>
-                </button>
+                <SettingsData type='PAX' />
+              </li>
+
+              <li className={`${styles.startBlock} ${styles.pax}`}>
+                <div>
+                  <h4 className={`${styles.startInnerTitle} ${styles.paxTitle}`}>Дополнительные бонусы</h4>
+                  <p className={`${styles.startInnerText} ${styles.paxText}`}>Вы можете выключить - включить PAX, чтобы что-то...</p>
+                </div>
+                <SettingsData type='AdditionalBonuses' />
+              </li>
+
+              <li className={`${styles.startBlock} ${styles.pax}`}>
+                <div>
+                  <h4 className={`${styles.startInnerTitle} ${styles.paxTitle}`}>Ведение crm</h4>
+                  <p className={`${styles.startInnerText} ${styles.paxText}`}>Вы можете выключить - включить PAX, чтобы что-то...</p>
+                </div>
+                <SettingsData type='CrmManagement' />
               </li>
             </ul>
           </div>
