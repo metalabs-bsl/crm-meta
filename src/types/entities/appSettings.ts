690bb839f1ae8cb619b4cecee87d292d83ab3b6d
Mikhaylov-M <mikhaylov.m@outlook.com>
2024-10-18 18:13:35 +0600

    get, create, delete for brands and products work. Update dont work, fix need

--- src/types/entities/appSettings.ts
+++ src/types/entities/appSettings.ts
@@ -5,3 +5,11 @@ export interface IResAppSettings {
 export interface IUpdateAppSettings {
   is_calculator_open: boolean;
 }
+
+export interface ISettingsCalculatorBrandData {
+  id: string;
+  created_at?: string;
+  updated_at?: string;
+  name: string;
+  isEditing?: boolean;
+}
